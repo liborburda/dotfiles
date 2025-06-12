6c33d18f24da413571de0378d6a81922d5f02942
Libor Burda <4998783+liborburda@users.noreply.github.com>
2025-06-12 16:06:44 +0200

    Configure color for vim-better-whitespace

--- nvim/lua/plugins/vim-better-whitespace.lua
+++ nvim/lua/plugins/vim-better-whitespace.lua
@@ -1,5 +1,8 @@
 return {
   {
-    "ntpeters/vim-better-whitespace"
+    "ntpeters/vim-better-whitespace",
+    config = function()
+      vim.api.nvim_set_hl(0, "ExtraWhitespace", { bg = "#ed8796" })
+    end
   }
 }
